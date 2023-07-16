2a0b3b61488e37f10bbdf92126d90735df4341ed
thanhdt2706 <137756323+thanhdt2706@users.noreply.github.com>
2023-07-16 13:59:10 +0700

    done unitest profilecontroller

--- profileservice/src/test/java/com/tanthanh/profileservice/ProfileControllerTest.java
+++ profileservice/src/test/java/com/tanthanh/profileservice/ProfileControllerTest.java
@@ -1,5 +1,6 @@
 package com.tanthanh.profileservice;
 
+import com.google.gson.Gson;
 import com.tanthanh.commonservice.utils.Constant;
 import com.tanthanh.profileservice.controller.ProfileController;
 import com.tanthanh.profileservice.model.ProfileDTO;
@@ -13,7 +14,10 @@ import org.mockito.Mock;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.util.ReflectionTestUtils;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(SpringExtension.class)
@@ -25,15 +29,29 @@ class ProfileControllerTest {
     private ProfileController profileController;
 
     private ProfileDTO profileDTO;
+
+    Gson gson = new Gson();
     @BeforeEach
     public void setUp(){
-        profileDTO = new ProfileDTO(1,"test@gmail.com", Constant.STATUS_PROFILE_ACTIVE,200,"name","ADMIN");
+        profileDTO = new ProfileDTO(1,"test@gmail.com", Constant.STATUS_PROFILE_ACTIVE,200,"name","CUSTOMER");
         ReflectionTestUtils.setField(profileController,"profileService",profileService);
     }
     @Test
     void getAllProfileSuccess(){
         when(profileService.getAllProfile()).thenReturn(Flux.just(profileDTO));
         profileController.getAllProfile().getBody().
+                doOnNext(profileDTO1 -> Assertions.assertEquals(profileDTO,profileDTO1)).subscribe();
+    }
+    @Test
+    void checkDuplicate(){
+        when(profileService.checkDuplicate(anyString())).thenReturn(Mono.just(true));
+        profileController.checkDuplicate(anyString()).getBody().
+                doOnNext(aBoolean -> Assertions.assertEquals(aBoolean,true)).subscribe();
+    }
+    @Test
+    void createNewProfile(){
+        when(profileService.createNewProfile(any(ProfileDTO.class))).thenReturn(Mono.just(profileDTO));
+        profileController.createNewProfile(gson.toJson(profileDTO)).getBody().
                 doOnNext(profileDTO1 -> Assertions.assertEquals(profileDTO,profileDTO1)).subscribe();
     }
 }
